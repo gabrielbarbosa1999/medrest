7f57ffe16cfbfdc7e06f7e513371941fced35eb1
Gabriel Barbosa <gabrielbarbosaoliveira11999@gmail.com>
2022-11-25 23:58:21 -0300

    Iniciado Projeto

---
+++ README.md
@@ -0,0 +1,5 @@
+**MedRest**
+
+Uma API construída em Java usando o Spring Boot, simulando um sistema de agendamento de consultado em uma clina medica.
+
+Com um CRUD de Médicos, Pacientes e conta com Agendamento.
---
+++ src/main/java/dev/gabrielbarbosa/api/MedRestApplication.java
@@ -0,0 +1,13 @@
+package dev.gabrielbarbosa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedRestApplication.class, args);
+	}
+
+}
